f3c21cf308c67296a73cfce5849385509517d4db
Kyle Deal <kdeal@kyledeal.com>
2017-09-24 23:43:36 -0700

    Added basic cli to help with setup and adding new repos

--- src/config.rs
+++ src/config.rs
@@ -1,38 +1,61 @@
 use std::collections::BTreeMap;
+use std::env::home_dir;
+use std::fs::create_dir_all;
 use std::fs::File;
 use std::io::Read;
 use std::io::Write;
+use std::path::PathBuf;
 use toml;
 use toml::value::Table;
 use toml::value::Value;
 
-#[derive(Clone, Deserialize, Debug, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Repo {
     pub message: Option<String>,
     pub sha: Option<String>,
 }
 
-#[derive(Clone, Deserialize, Debug, Serialize)]
+#[derive(Clone, Default, Deserialize, Debug, Serialize)]
 pub struct Config {
-    pub github_token: String,
-    pub todoist_token: String,
+    pub github_token: Option<String>,
+    pub todoist_token: Option<String>,
 }
 
-type Repos = BTreeMap<String, Repo>;
+pub type Repos = BTreeMap<String, Repo>;
 
-pub fn load() -> (Repos, Option<Config>) {
-    let mut config_file = File::open("./repos.toml").expect("file not found");
+fn expand_config_path<'a>(config_file: &'a str) -> PathBuf {
+    let mut path = String::from(config_file);
+    if path.starts_with("~") {
+        // TODO: Make this better
+        path.remove(0);
+        path.remove(0);
+        let mut home_dir = home_dir().expect("Can't get home directory");
+        home_dir.push(&path);
+        return home_dir;
+    }
+    PathBuf::from(config_file)
+}
+
+pub fn load<'a>(config_file: &'a str) -> (Repos, Config) {
+    let mut repos: Repos = BTreeMap::new();
+    let mut config = Config::default();
+
+    let config_file = expand_config_path(config_file);
+    let config_file = config_file.as_path();
+    if !config_file.exists() {
+        return (repos, config);
+    }
+    let mut config_file = File::open(config_file).expect("file not found");
 
     let mut contents = String::new();
     config_file.read_to_string(&mut contents).expect(
         "something went wrong reading the file",
     );
-    let mut repos: Repos = BTreeMap::new();
     let table: Table = toml::from_str(&contents).unwrap();
-    let mut config = None;
+
     for (name, repo) in table {
         if name == "config" {
-            config = Some(repo.try_into().unwrap());
+            config = repo.try_into().unwrap();
         } else {
             let repo: Repo = repo.try_into().unwrap();
             repos.insert(name, repo);
@@ -41,14 +64,19 @@ pub fn load() -> (Repos, Option<Config>) {
     (repos, config)
 }
 
-pub fn save(repos: Repos, config: Config) {
+pub fn save<'a>(config_file: &'a str, repos: Repos, config: Config) {
     let mut config_table: Table = Table::new();
     config_table.insert("config".to_string(), Value::try_from(config).unwrap());
 
     let config_contents = toml::to_string_pretty(&config_table).unwrap();
     let repos_contents = toml::to_string_pretty(&repos).unwrap();
 
-    let mut config_file = File::create("./repos.toml").expect("couldn't open file for write");
+    let config_file = expand_config_path(config_file);
+    let config_file = config_file.as_path();
+    create_dir_all(config_file.parent().unwrap()).unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+    let mut config_file = File::create(config_file).expect("couldn't open file for write");
     config_file.write_all(config_contents.as_bytes()).expect(
         "failed to write config",
     );
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,92 @@
+extern crate clap;
 extern crate github_rs;
 extern crate reqwest;
 extern crate serde;
+extern crate rpassword;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+#[macro_use]
+extern crate text_io;
 extern crate toml;
 
+use std::io::{self, Write};
+
+use clap::App;
+use clap::Arg;
+use clap::ArgMatches;
+use clap::SubCommand;
 use github_rs::client::Github;
+use rpassword::prompt_password_stdout;
 
 mod config;
 mod github;
 mod todoist;
 
+use config::Config;
+use config::Repo;
+use config::Repos;
 use todoist::Task;
 use todoist::NewTask;
 use todoist::NewComment;
 
 
+fn get_args<'a>() -> ArgMatches<'a> {
+    App::new("repo-notify")
+        .version("0.1.0")
+        .about("Add todoist task if Github repositories are updated")
+        .author("Kyle D. <kdeal@kyledeal.com>")
+        .arg(
+            Arg::with_name("config_file")
+                .help("Config file for repo-notify")
+                .default_value("~/.config/repo-notify.toml")
+                .long("config-file")
+                .short("c")
+                .takes_value(true),
+        )
+        .subcommand(
+            SubCommand::with_name("add")
+                .about("Add a repository to the watch list")
+                .arg(
+                    Arg::with_name("repository")
+                        .help("Repository to add")
+                        .required(true)
+                        .takes_value(true),
+                ),
+        )
+        .subcommand(SubCommand::with_name("check").about(
+            "Check for repo updates",
+        ))
+        .subcommand(SubCommand::with_name("setup").about("Do the initial setup"))
+        .get_matches()
+}
+
 fn main() {
-    let (mut repos, config) = config::load();
-    if let None = config {
-        println!("Must set token in config");
-        return;
-    }
-    let config = config.unwrap();
+    let args = get_args();
+    let config_file = args.value_of("config_file").unwrap();
+    let (repos, config) = config::load(config_file);
+    let (repos, config) = match args.subcommand() {
+        ("add", Some(sub_args)) => (add(sub_args, repos), config),
+        ("check", Some(_)) => check(repos, config),
+        ("setup", Some(_)) => setup(repos, config),
+        _ => (repos, config),
+    };
+    config::save(config_file, repos, config);
+}
 
+fn add<'a>(args: &'a ArgMatches, mut repos: Repos) -> Repos {
+    let repo_name = args.value_of("repository").unwrap().to_string();
+    println!("Adding {} to watched repositories", repo_name);
+    repos.insert(repo_name, Repo::default());
+    repos
+}
+
+fn check(mut repos: Repos, config: Config) -> (Repos, Config) {
     // TODO: Make a client instead
-    let tasks = todoist::get_tasks(&config.todoist_token);
+    let todoist_token = &config.todoist_token.clone().unwrap();
+    let tasks = todoist::get_tasks(todoist_token);
 
-    let client = Github::new(config.github_token.as_str()).unwrap();
+    let client = Github::new(config.github_token.clone().unwrap().as_str()).unwrap();
     for (name, repo) in repos.iter_mut() {
         let split: Vec<&str> = name.split('/').collect();
         let owner = split[0];
@@ -48,12 +106,28 @@ fn main() {
                 gh_commit.commit.message.to_string(),
                 gh_commit.sha.to_string(),
                 &tasks,
-                &config.todoist_token,
+                todoist_token,
             );
             println!("{} was updated", name);
         }
     }
-    config::save(repos, config);
+    (repos, config)
+}
+
+fn setup(mut repos: Repos, mut config: Config) -> (Repos, Config) {
+    // TODO: Make this work for updating single token
+    let token = prompt_password_stdout("Github Token: ").unwrap();
+    config.github_token = Some(token);
+    let token = prompt_password_stdout("Todoist Token: ").unwrap();
+    config.todoist_token = Some(token);
+
+    if repos.is_empty() {
+        print!("Repository: ");
+        io::stdout().flush().unwrap();
+        let repository: String = read!("{}\n");
+        repos.insert(repository, Repo::default());
+    }
+    (repos, config)
 }
 
 fn report_update(
